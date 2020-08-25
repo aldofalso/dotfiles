5af4287e55dcbeb4dab1212c4e9a5e7e4a8cd38e
aldo0215 <45566380+aldo0215@users.noreply.github.com>
2020-08-25 08:29:09 +0200

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,18 +1,22 @@
 #!/bin/sh
 ##
+# ~/config.sh
 
 sudo xbps-install -Su
 
-sudo xbps-install -S slock dmenu xsetroot xorg-minimal xf86-video-amdgpu xinit xorg-server xss-lock xss-lock vscode volumeicon alsa-utils alsa-lib pcmanfm file-roller gvfs gvfs-mtp feh scrot mpv lxappearance picom dejavu-fonts-ttf terminus-font redshift gimp libreoffice man android-tools firefox telegram-desktop git youtube-dl ffmpeg psmisc xset
+sudo xbps-install -S slock dmenu xsetroot xorg-minimal xf86-video-amdgpu xinit xorg-server xss-lock xss-lock vscode volumeicon alsa-utils alsa-lib pcmanfm file-roller gvfs gvfs-mtp feh scrot mpv lxappearance picom dejavu-fonts-ttf redshift gimp libreoffice man android-tools firefox telegram-desktop git youtube-dl ffmpeg psmisc xset android-file-transfer-linux cmatrix ytop
 
 mkdir /home/aldo/.config/redshift
 mkdir /home/aldo/.config/volumeicon
 mkdir /home/aldo/.config/picom/
 mkdir /home/aldo/wallpapers
 
+sudo chmod +x .dwmbar
+
+cp /home/aldo/dotfiles/.dwmbar /home/aldo/
 cp /home/aldo/dotfiles/etc/xdg/picom.conf /home/aldo/.config/picom
 cp /home/aldo/dotfiles/.xinitrc /home/aldo/
-cp /home/aldo/dotfiles/.bashrc /home/aldo
+cp /home/aldo/dotfiles/.bashrc /home/aldo/
 cp /home/aldo/dotfiles/config/redshift/redshift.conf /home/aldo/.config/redshift
 cp /home/aldo/dotfiles/config/volumeicon/volumeicon /home/aldo/.config/volumeicon
-cp /home/aldo/dotfiles/wallpapers/wall.jpg /home/aldo/wallpapers
+cp /home/aldo/dotfiles/wallpapers/wall.jpg /home/aldo/wallpapers/
