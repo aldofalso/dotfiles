5419fb8d804bb4d20deb46e7c722e4ad364baad0
aldofalso <aldo.falso@outlook.com>
2020-08-25 12:12:11 +0200

    package fixed

--- config.sh
+++ config.sh
@@ -4,7 +4,7 @@
 
 sudo xbps-install -Su
 
-sudo xbps-install -S slock dmenu xsetroot xorg-minimal xf86-video-amdgpu xinit xorg-server xss-lock xss-lock vscode volumeicon alsa-utils alsa-lib pcmanfm file-roller gvfs gvfs-mtp feh scrot mpv lxappearance picom dejavu-fonts-ttf redshift gimp libreoffice man android-tools firefox telegram-desktop git youtube-dl ffmpeg psmisc xset android-file-transfer-linux cmatrix htop virtualbox-ose bash-completion vim nano lm_sensors font-awesome
+sudo xbps-install -S slock dmenu xsetroot xorg-minimal xf86-video-amdgpu xinit xorg-server xss-lock xss-lock vscode volumeicon alsa-utils alsa-lib pcmanfm file-roller gvfs gvfs-mtp feh scrot mpv lxappearance picom dejavu-fonts-ttf redshift gimp libreoffice man android-tools firefox telegram-desktop git youtube-dl ffmpeg psmisc xset android-file-transfer-linux cmatrix htop virtualbox-ose bash-completion vim nano lm_sensors font-awesome5
 
 mkdir /home/aldo/.config/redshift
 mkdir /home/aldo/.config/volumeicon
