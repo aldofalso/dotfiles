a2a65a5c7e8cb96d0a5a6aaa0c98f95fc247c027
aldofalso <aldo.falso@outlook.com>
2020-08-25 10:49:31 +0200

    fix

--- config.sh
+++ config.sh
@@ -11,8 +11,6 @@ mkdir /home/aldo/.config/volumeicon
 mkdir /home/aldo/.config/picom/
 mkdir /home/aldo/wallpapers
 
-chmod +rx .dwmbar
-
 cp /home/aldo/dotfiles/.dwmbar /home/aldo/
 cp /home/aldo/dotfiles/etc/xdg/picom.conf /home/aldo/.config/picom
 cp /home/aldo/dotfiles/.xinitrc /home/aldo/
@@ -20,3 +18,5 @@ cp /home/aldo/dotfiles/.bashrc /home/aldo/
 cp /home/aldo/dotfiles/config/redshift/redshift.conf /home/aldo/.config/redshift
 cp /home/aldo/dotfiles/config/volumeicon/volumeicon /home/aldo/.config/volumeicon
 cp /home/aldo/dotfiles/wallpapers/wall.jpg /home/aldo/wallpapers/
+
+chmod +rx /home/aldo/.dwmbar
