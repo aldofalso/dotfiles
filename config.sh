2dfae54c6e85c72de0dd5666d83c37ddcde0cfd7
aldofalso <aldo.falso@outlook.com>
2020-09-04 12:04:46 +0200

    removed bloat in dwm status and stuff

--- config.sh
+++ config.sh
@@ -4,7 +4,7 @@
 
 sudo xbps-install -Su
 
-sudo xbps-install -S slock dmenu xsetroot xorg-minimal xorg-video-drivers xinit xorg-server xss-lock xss-lock vscode alsa-utils alsa-lib pcmanfm file-roller gvfs gvfs-mtp feh scrot mpv lxappearance ufetch picom dejavu-fonts-ttf redshift gimp libreoffice man android-tools chromium telegram-desktop git youtube-dl ffmpeg psmisc xset android-file-transfer-linux cmatrix htop virtualbox-ose bash-completion vim nano lm_sensors font-awesome5 mesa-vaapi mesa-vdpau vulkan-loader amdvlk NetworkManager curl elogind sxhkd galculator breeze-obsidian-cursor-theme gnome-themes-standard breeze-icons base-devel libX11-devel libXft-devel libXinerama-devel
+sudo xbps-install -S slock dmenu xsetroot xorg-minimal xorg-video-drivers xinit xorg-server xss-lock xss-lock vscode alsa-utils alsa-lib pcmanfm file-roller gvfs gvfs-mtp feh scrot mpv lxappearance ufetch picom dejavu-fonts-ttf redshift gimp libreoffice man android-tools chromium telegram-desktop git youtube-dl ffmpeg psmisc xset android-file-transfer-linux cmatrix htop virtualbox-ose bash-completion vim nano lm_sensors mesa-vaapi mesa-vdpau vulkan-loader amdvlk NetworkManager curl elogind sxhkd galculator-gtk3 breeze-snow-cursor-theme gnome-themes-standard breeze-icons base-devel libX11-devel libXft-devel libXinerama-devel
 
 #config files
 mkdir /home/aldo/.config
@@ -20,7 +20,7 @@ cp /home/aldo/dotfiles/.bashrc /home/aldo/
 cp /home/aldo/dotfiles/config/picom/picom.conf /home/aldo/.config/picom/
 cp /home/aldo/dotfiles/config/redshift/redshift.conf /home/aldo/.config/redshift
 cp /home/aldo/dotfiles/wallpapers/wall.jpg /home/aldo/wallpapers/
-cp /home/aldo/dotfiles/sxhkdrc /home/aldo/.config/sxhkd/
+cp /home/aldo/dotfiles/config/sxhkd/sxhkdrc /home/aldo/.config/sxhkd/
 
 #autostart
 sudo ln -s /etc/sv/NetworkManager /var/service
