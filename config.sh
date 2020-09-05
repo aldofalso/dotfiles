42455f301eb0cd0e850df740545f23418a42ee88
aldofalso <aldo.falso@outlook.com>
2020-09-05 12:30:10 +0200

    updated config.sh

--- config.sh
+++ config.sh
@@ -2,33 +2,36 @@
 ##
 # ~/config.sh
 
-sudo xbps-install -Su
+ sudo xbps-install -Su
 
-sudo xbps-install -S slock dmenu xsetroot xorg-minimal xorg-video-drivers xinit xorg-server xss-lock xss-lock vscode alsa-utils alsa-lib pcmanfm file-roller gvfs gvfs-mtp feh scrot mpv ufetch picom dejavu-fonts-ttf redshift gimp libreoffice man android-tools chromium telegram-desktop git youtube-dl ffmpeg psmisc xset cmatrix htop virtualbox-ose bash-completion vim nano lm_sensors mesa-vaapi mesa-vdpau vulkan-loader amdvlk NetworkManager curl elogind sxhkd galculator-gtk3 breeze-snow-cursor-theme gnome-themes-standard breeze-icons base-devel libX11-devel libXft-devel libXinerama-devel terminus-font noto-fonts-emoji
+ sudo xbps-install -S slock dmenu xsetroot xorg-minimal xorg-video-drivers xinit xorg-server xss-lock xss-lock vscode alsa-utils alsa-lib pcmanfm file-roller gvfs gvfs-mtp feh scrot mpv ufetch picom dejavu-fonts-ttf redshift gimp libreoffice man android-tools chromium telegram-desktop git youtube-dl ffmpeg psmisc xset cmatrix htop virtualbox-ose bash-completion vim nano lm_sensors mesa-vaapi mesa-vdpau vulkan-loader amdvlk NetworkManager curl elogind sxhkd galculator-gtk3 breeze-snow-cursor-theme gnome-themes-standard breeze-icons base-devel libX11-devel libXft-devel libXinerama-devel terminus-font noto-fonts-emoji
 
-#config files
-mkdir /home/aldo/.config
-mkdir /home/aldo/.config/redshift/
-mkdir /home/aldo/.config/sxhkd/
-mkdir /home/aldo/.config/picom/
-mkdir /home/aldo/wallpapers/
+  #config files
+  mkdir /home/aldo/.config
+  mkdir /home/aldo/.config/redshift/
+  mkdir /home/aldo/.config/sxhkd/
+  mkdir /home/aldo/.config/picom/
+  mkdir /home/aldo/wallpapers/
 
-sudo cp -r /home/aldo/dotfiles/rules/49-nopasswd_global.rules /etc/polkit-1/rules.d/
-cp /home/aldo/dotfiles/.dwmbar /home/aldo/
-cp /home/aldo/dotfiles/.xinitrc /home/aldo/
-cp /home/aldo/dotfiles/.bashrc /home/aldo/
-cp /home/aldo/dotfiles/config/picom/picom.conf /home/aldo/.config/picom/
-cp /home/aldo/dotfiles/config/redshift/redshift.conf /home/aldo/.config/redshift
-cp /home/aldo/dotfiles/wallpapers/wall.jpg /home/aldo/wallpapers/
-cp /home/aldo/dotfiles/config/sxhkd/sxhkdrc /home/aldo/.config/sxhkd/
-cp -r /home/aldo/dotfiles/config/.icons /home/aldo
-cp -r /home/aldo/dotfiles/config/gtk-3.0 /home/aldo/.config
+ sudo cp -r /home/aldo/dotfiles/rules/49-nopasswd_global.rules /etc/polkit-1/rules.d/
+ cp -r /home/aldo/dotfiles/.dwmbar /home/aldo/ 
+ cp -r /home/aldo/dotfiles/.xinitrc /home/aldo/
+ cp -r /home/aldo/dotfiles/.bashrc /home/aldo/
+ cp -r /home/aldo/dotfiles/config/picom/picom.conf /home/aldo/.config/picom/
+ cp -r /home/aldo/dotfiles/config/redshift/redshift.conf /home/aldo/.config/redshift
+ cp -r /home/aldo/dotfiles/wallpapers/wall.jpg /home/aldo/wallpapers/
+ cp -r /home/aldo/dotfiles/config/sxhkd/sxhkdrc /home/aldo/.config/sxhkd/
+ cp -r /home/aldo/dotfiles/config/.icons /home/aldo
+ cp -r /home/aldo/dotfiles/config/gtk-3.0 /home/aldo/.config
 
-#autostart
-sudo ln -s /etc/sv/NetworkManager /var/service
-sudo ln -s /etc/sv/dbus/ /var/service
+  #autostart
+  sudo ln -s /etc/sv/NetworkManager /var/service
+  sudo ln -s /etc/sv/dbus/ /var/service
 
-sudo rm -rf /var/service/wpa_supplicant
-sudo rm -rf /var/service/dhcpcd
-
-chmod +rx /home/aldo/.dwmbar
+  sudo rm -rf /var/service/wpa_supplicant
+  sudo rm -rf /var/service/dhcpcd
+  
+  #exec
+  chmod +rx /home/aldo/.dwmbar
+ 
+  #done
