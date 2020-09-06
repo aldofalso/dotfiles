60ccc48d4525453611fccc181e7c84d27fad6035
aldofalso <aldofalso02@gmail.com>
2020-09-06 22:40:44 +0200

    new tty font

--- config.sh
+++ config.sh
@@ -4,6 +4,7 @@
 
 sudo xbps-install -Su
 
+ #packages
  sudo xbps-install -S slock dmenu xsetroot xorg-minimal xorg-video-drivers xinit xorg-server xss-lock xss-lock vscode alsa-utils alsa-lib pcmanfm file-roller gvfs gvfs-mtp feh scrot mpv ufetch picom dejavu-fonts-ttf redshift gimp libreoffice man android-tools chromium telegram-desktop git youtube-dl ffmpeg psmisc xset cmatrix htop qemu bash-completion vim nano lm_sensors mesa-vaapi mesa-vdpau vulkan-loader amdvlk NetworkManager curl elogind sxhkd galculator-gtk3 breeze-snow-cursor-theme gnome-themes-standard breeze-icons base-devel libX11-devel libXft-devel libXinerama-devel terminus-font noto-fonts-emoji go-mtpfs
 
   #config files
@@ -12,7 +13,8 @@ sudo xbps-install -Su
   mkdir /home/aldo/.config/sxhkd/
   mkdir /home/aldo/.config/picom/
   mkdir /home/aldo/wallpapers/
-
+ 
+  #cp files
  sudo cp -r /home/aldo/dotfiles/rules/49-nopasswd_global.rules /etc/polkit-1/rules.d/
  cp -r /home/aldo/dotfiles/.dwmbar /home/aldo/ 
  cp -r /home/aldo/dotfiles/.xinitrc /home/aldo/
@@ -33,5 +35,8 @@ sudo xbps-install -Su
   
   #exec
   chmod +rx /home/aldo/.dwmbar
- 
+
+  #font in tty
+  sudo sed -i 's/#FONT="lat9w-16"/FONT="ter-v32n"/g' /etc/rc.conf
+
  #done
