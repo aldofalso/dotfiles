5c1b8fea6e5a552080a67acef5f761a5f01f0e60
aldofalso <aldofalso02@gmail.com>
2020-09-08 08:18:38 +0200

    addedd dmenuclipboard & dmenulogout

--- install.sh
+++ install.sh
@@ -5,23 +5,24 @@
 sudo xbps-install -Su
 
  #packages
- sudo xbps-install -S slock dmenu xsetroot xclip xorg-minimal xorg-video-drivers xinit xorg-server xss-lock xss-lock alsa-utils alsa-lib pcmanfm file-roller gvfs gvfs-mtp feh scrot mpv ufetch picom dejavu-fonts-ttf redshift gimp libreoffice man android-tools chromium telegram-desktop git youtube-dl ffmpeg psmisc xset cmatrix htop qemu bash-completion neovim lm_sensors mesa-vaapi mesa-vdpau vulkan-loader amdvlk NetworkManager curl elogind sxhkd galculator-gtk3 breeze-snow-cursor-theme gnome-themes-standard breeze-icons base-devel libX11-devel libXft-devel libXinerama-devel terminus-font noto-fonts-emoji go-mtpfs
+ sudo xbps-install -S slock dmenu xsetroot xsel unclutter-xfixes xorg-minimal xorg-video-drivers xinit xorg-server xss-lock xss-lock alsa-utils alsa-lib pcmanfm file-roller gvfs gvfs-mtp feh scrot mpv ufetch picom dejavu-fonts-ttf redshift gimp libreoffice man android-tools chromium telegram-desktop git youtube-dl ffmpeg psmisc xset cmatrix htop qemu bash-completion neovim lm_sensors mesa-vaapi mesa-vdpau vulkan-loader amdvlk NetworkManager curl elogind sxhkd galculator-gtk3 breeze-snow-cursor-theme gnome-themes-standard breeze-icons base-devel libX11-devel libXft-devel libXinerama-devel terminus-font noto-fonts-emoji go-mtpfs clipmenu clipnotify
 
   #config files
   mkdir /home/aldo/.config
 
   #cp files
  sudo cp -r /home/aldo/dotfiles/rules/49-nopasswd_global.rules /etc/polkit-1/rules.d/
+ sudo cp -r /home/aldo/dotfiles/scripts/dmenulogout /usr/bin/
  cp -r /home/aldo/dotfiles/.dwmbar /home/aldo/ 
  cp -r /home/aldo/dotfiles/.xinitrc /home/aldo/
  cp -r /home/aldo/dotfiles/.bashrc /home/aldo/
  cp -r /home/aldo/dotfiles/config/picom/ /home/aldo/.config/
  cp -r /home/aldo/dotfiles/config/redshift/ /home/aldo/.config/
  cp -r /home/aldo/dotfiles/wallpapers/ /home/aldo/
- cp -r /home/aldo/dotfiles/config/sxhkd/ /home/aldo/.config/
- cp -r /home/aldo/dotfiles/config/.icons /home/aldo/
- cp -r /home/aldo/dotfiles/config/gtk-3.0 /home/aldo/.config/
- cp -r /home/aldo/dotfiles/config/nvim /home/aldo/.config/
+ cp -r /home/aldo/dotfiles/config/sxhkd /home/aldo/.config/
+ cp -r /home/aldo/dotfiles/config/.icons/ /home/aldo/
+ cp -r /home/aldo/dotfiles/config/gtk-3.0/ /home/aldo/.config/
+ cp -r /home/aldo/dotfiles/config/nvim/ /home/aldo/.config/
 
   #autostart
   sudo ln -s /etc/sv/NetworkManager /var/service
